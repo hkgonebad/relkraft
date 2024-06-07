96789b04b62933f4c326e982f74b569bfb916e01
sapande-1719 <sapande1719@gmail.com>
2024-06-07 15:29:54 +0530

    refactor: minor updates

--- js/main.js
+++ js/main.js
@@ -1,42 +1,31 @@
 // document.addEventListener("DOMContentLoaded", yall);
 //AOS
-// AOS.init({
-//     duration: 600
-// });
+AOS.init();
 
 // Get the header
-var header = document.getElementById("header");
-var body = document.body;
+// var header = document.getElementById("header");
+// var body = document.body;
 
-if (header) {
-  // Get the offset position of the navbar
-  var sticky = header.offsetTop;
+// if (header) {
+//   // Get the offset position of the navbar
+//   var sticky = header.offsetTop;
 
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function updateStickyHeader() {
-    if (window.pageYOffset > sticky) {
-      body.classList.add("sticky");
-    } else {
-      body.classList.remove("sticky");
-    }
-    updateLogoImage();
-  }
+//   // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+//   function updateStickyHeader() {
+//     if (window.pageYOffset > sticky) {
+//       body.classList.add("sticky");
+//     } else {
+//       body.classList.remove("sticky");
+//     }
+//     // updateLogoImage();
+//   }
 
-  // Use an event listener instead of onscroll property
-  window.addEventListener("scroll", function () {
-    // Use requestAnimationFrame for performance
-    window.requestAnimationFrame(updateStickyHeader);
-  });
-}
-
-// Update the logo image based on the sticky and home page classes
-function updateLogoImage() {
-  const isHomePage = $("body").hasClass("home");
-  const isSticky = $("body").hasClass("sticky");
-  const isRegister = $("body").hasClass("regPage");
-  const logoImgSrc = isHomePage && !isSticky ? "img/logo-white.png" : "img/logo.png";
-  // $(".logo").find("img").attr("src", logoImgSrc);
-}
+//   // Use an event listener instead of onscroll property
+//   window.addEventListener("scroll", function () {
+//     // Use requestAnimationFrame for performance
+//     window.requestAnimationFrame(updateStickyHeader);
+//   });
+// }
 
 //
 //new WOW().init();
@@ -45,10 +34,6 @@ function updateLogoImage() {
 
 ///////////////////////////////////
 $(function () {
-  // Push Menu
-  const overlay = $(".sidebar-overlay");
-  const navMain = $("#navMain");
-
   // Header
   $("#header").load("header.html", function () {
     // ScrollAnchor
@@ -91,30 +76,30 @@ $(function () {
   }
 
   // Brands Slider
-  if ($(".brandSlider").length > 0) {
-    $(".brandSlider").slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      centerMode: true,
-      variableWidth: true,
-      dots: true,
-      autoplay: true,
-      autoplayDuration: 2000,
-      arrows: true,
-      lazyLoad: "ondemand",
-      prevArrow:
-        "<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
-      nextArrow:
-        "<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>",
+  // if ($(".brandSlider").length > 0) {
+  //   $(".brandSlider").slick({
+  //     slidesToShow: 3,
+  //     slidesToScroll: 1,
+  //     centerMode: true,
+  //     variableWidth: true,
+  //     dots: true,
+  //     autoplay: true,
+  //     autoplayDuration: 2000,
+  //     arrows: true,
+  //     lazyLoad: "ondemand",
+  //     prevArrow:
+  //       "<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
+  //     nextArrow:
+  //       "<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>",
 
-      responsive: [
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 1,
-          },
-        },
-      ],
-    });
-  }
+  //     responsive: [
+  //       {
+  //         breakpoint: 700,
+  //         settings: {
+  //           slidesToShow: 1,
+  //         },
+  //       },
+  //     ],
+  //   });
+  // }
 });
